8d8491cd1730084c98e8888d239d99d769963ada
Fabio Colombo Ely <fcolomboely@gmail.com>
2013-07-11 00:57:35 -0300

    Added option for getting file from URL

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,10 +66,27 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of the file to be checked')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if ( program.url ) {
+        var currentTime = new Date().toISOString().replace(/\-/, '').replace(/\-/, '').replace(/T/, '').replace(/\:/, '').replace(/\:/, '').replace(/\..+/, '');
+        var fileName = 'download' + currentTime + '.html';
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                console.error('Error getting file from URL... Aborting');
+                process.exit(1);
+            } else {
+                fs.writeFileSync(fileName, result);
+                var checkJson = checkHtmlFile(fileName, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            }
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    };
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
